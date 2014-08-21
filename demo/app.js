5b4209b8b759c897c1d7f66f90c3bf132f7035b0
Tyler Mendenhall <tylermendenhall@gmail.com>
2014-08-21 09:20:06 -0400

    Add new roles

--- demo/app.js
+++ demo/app.js
@@ -6,8 +6,9 @@ var myApp = angular.module('myApp',['multi-select'])
   $scope.roles = [
     {roleId: 1, roleName: "Administrator", roleDescription: "Can do a bunch of stuff",mx:[]},
     {roleId: 2, roleName: "Super User", roleDescription: "Ultimate power!",mx:[]},
-    {roleId:3, roleName:"Unlimited Cosmic Power", roleDescription:"Ultimate power",mx:[4]},
-    {roleId:4, roleName:"Itty Bitty Living Space", roleDescription:"a tiny bottle",mx:[3]}
+    {roleId:3, roleName:"Unlimited Cosmic Power", roleDescription:"Ultimate power",mx:[4,5]},
+    {roleId:4, roleName:"Itty Bitty Living Space", roleDescription:"a tiny bottle",mx:[3,5]},
+    {roleId:5, roleName:"Normal User", roleDescription:"a normal user",mx:[3,4]}
   ];
   
   $scope.user = {
